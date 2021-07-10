9f9d40db7b303aa6bf88d5291e9caf00f1fad093
lemstra <sebastian.lemstra@disy.net>
2021-07-10 23:00:21 +0200

    massive cleanup

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from piece import Piece, GraphicalPiece
 
-class GraphicalBoard ():
+
+class GraphicalBoard:
     def __init__(self, rows, cols, display_variables):
         self.rows = rows
         self.cols = cols
@@ -14,7 +15,7 @@ class GraphicalBoard ():
                 if self.graphicalboard[i][j] != 0 and self.graphicalboard[i][j].getType() != 0:
                     self.graphicalboard[i][j].draw(gameDisplay)
 
-    def importBoard (self, board):
+    def importBoard(self, board):
         i = 0
         while i < 64:
             if board.getPiece(i) != 0:
@@ -24,66 +25,58 @@ class GraphicalBoard ():
             self.graphicalboard[graphicalPiece.row][graphicalPiece.col] = graphicalPiece
             i += 1
 
-
     def getPiece(self, row, col):
         return self.graphicalboard[row][col]
 
     def setPiece(self, graphicalPiece, row, col):
         self.graphicalboard[row][col] = graphicalPiece
-        #return self.graphicalboard
 
 
-class Board():
+class Board:
     def __init__(self):
         self.b = [0] * 64
-        self.dense_b ={}
+        self.dense_b = {}
 
     def importBoard(self, board):
         for i in range(8):
             for j in range(8):
                 index = i * 8 + j
-                #print(index)
-                #print(i, j)
-                self.b[index] = board.getPiece(i,j)
-        #print(self.b)
+                self.b[index] = board.getPiece(i, j)
         return self.b
 
     def getPiece(self, index):
-        return(self.b[index])
+        return self.b[index]
 
     def setPiece(self, piece, index):
         self.b[index] = piece
 
-    def importFEN(self,fenString):
+    def importFEN(self, fenString):
         fenString = fenString
         fenParts = fenString.split(" ")
 
         boardIndex = 0
         for element in fenParts[0]:
-            #print(f"current BoardIndex: {boardIndex}")
             if element == '/':
                 pass
             elif element.isdigit():
                 boardIndex = boardIndex + int(element)
             else:
-                color = 16 #black color
-                if element.isupper() == True:
-                    color = 8 #white color
+                color = 16  # black color
+                if element.isupper():
+                    color = 8  # white color
                 if element.lower() == 'p':
-                    self.b[boardIndex] = color+1
+                    self.b[boardIndex] = color + 1
                 elif element.lower() == 'r':
-                    self.b[boardIndex] = color+2
+                    self.b[boardIndex] = color + 2
                 elif element.lower() == 'n':
-                    self.b[boardIndex] = color+3
+                    self.b[boardIndex] = color + 3
                 elif element.lower() == 'b':
-                    self.b[boardIndex] = color+4
+                    self.b[boardIndex] = color + 4
                 elif element.lower() == 'q':
-                    self.b[boardIndex] = color+5
+                    self.b[boardIndex] = color + 5
                 elif element.lower() == 'k':
-                    self.b[boardIndex] = color+6
-                #print(f"Found piece is a {color} {element}")
+                    self.b[boardIndex] = color + 6
                 boardIndex += 1
-        #print(f"Imported Board: {self.b}")
         return self.b
 
     def generateDenseBoard(self):
@@ -93,8 +86,4 @@ class Board():
             if self.b[index] != 0:
                 self.dense_b[index] = self.b[index]
             index += 1
-        #print(F"Dense Board: {self.dense_b}")
-        return(self.dense_b)
-
-
-
+        return self.dense_b
--- piece.py
+++ piece.py
@@ -3,11 +3,15 @@ import os
 import math
 
 
-class GraphicalPiece():
+def getBoardIndex(col, row):
+    index = row * 8 + col
+    return (index)
+
+
+class GraphicalPiece:
     def __init__(self, piece, fieldIndex, display_variables):
         self.row = math.floor(fieldIndex / 8)
         self.col = fieldIndex - self.row * 8
-        #print(f"create GraphicalPiece: {Piece(piece).getPieceType()}")
         self.color, self.type = Piece(piece).getPieceType()
         self.intType = Piece(piece).getIntPieceType()
         self.fieldsize = display_variables.get("fieldsize")
@@ -15,10 +19,10 @@ class GraphicalPiece():
         self.board_offset_y = display_variables.get("board_offset_y")
 
     def draw(self, gameDisplay):
-        #print("Image to draw: " + os.path.join("img", self.color + "_" + self.type + ".png"))
         if self.color != 'none':
-            drawThis=pygame.transform.scale(pygame.image.load(os.path.join("img", self.color + "_" + self.type + ".png")),
-                                            (self.fieldsize, self.fieldsize))
+            drawThis = pygame.transform.scale(
+                pygame.image.load(os.path.join("img", self.color + "_" + self.type + ".png")),
+                (self.fieldsize, self.fieldsize))
 
             x = self.col * self.fieldsize + self.board_offset_x
             y = self.row * self.fieldsize + self.board_offset_y
@@ -27,44 +31,19 @@ class GraphicalPiece():
 
     def getImageToDraw(self):
         if self.color != 'none':
-            drawThis=pygame.transform.scale(pygame.image.load(os.path.join("img", self.color + "_" + self.type + ".png")),
-                                            (self.fieldsize, self.fieldsize))
+            drawThis = pygame.transform.scale(
+                pygame.image.load(os.path.join("img", self.color + "_" + self.type + ".png")),
+                (self.fieldsize, self.fieldsize))
         return drawThis
 
     def getType(self):
         return self.type
 
     def getIntType(self):
-#        intType = 0
-#        if self.color == 'white':
-#            intType = 8
-#        else:
-#            intTpye = 16
-#        numberRepresentation = {
-#            "none": 0,
-#            "pawn": 1,
-#            "rook": 2,
-#            "knight": 3,
-#            "bishop": 4,
-#            "queen": 5,
-#            "king": 6
-#            # "white": 8,
-#            # "black": 16
-#        }
-#        for descr, num in numberRepresentation.items():
-#            if descr == self.type:
-#                intType = intType + num
-#                break
-#        return(intTpye)
-        return(self.intType)
+        return self.intType
 
 
-    def getBoardIndex(self, col, row):
-        index = row * 8 + col
-        return(index)
-
-
-class Piece():
+class Piece:
     def __init__(self, piece):
         self.piece = piece
 
@@ -77,8 +56,8 @@ class Piece():
             "bishop": 4,
             "queen": 5,
             "king": 6
-            #"white": 8,
-            #"black": 16
+            # "white": 8,
+            # "black": 16
         }
         tempPiece = self.piece
         if tempPiece > 15:
@@ -90,19 +69,11 @@ class Piece():
             color = 'white'
             tempPiece -= 8
 
-        #print(f"piece color is {color}")
-
         for descr, num in numberRepresentation.items():
             if num == tempPiece:
                 type = descr
-                #print(f"piece type is {type}")
                 break
-        #print(f"Piece is a {color} {type}")
-
-        return(color, type)
+        return color, type
 
     def getIntPieceType(self):
         return self.piece
-
-
-
